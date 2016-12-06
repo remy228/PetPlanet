3f446531aefb3934155f4255a66361f8053a096b
remy228 <remyamohanan91@gmail.com>
2016-12-06 13:41:11 -0800

    PostPet half done

--- app/src/main/java/com/example/remya/petplanet/PostPet.java
+++ app/src/main/java/com/example/remya/petplanet/PostPet.java
@@ -1,13 +1,25 @@
 package com.example.remya.petplanet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class PostPet extends AppCompatActivity {
+
+    ImageButton button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,33 @@ public class PostPet extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        button = (ImageButton) findViewById(R.id.photobutton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+}
+    private void openGallery()
+    {
+        Log.i("Image add on click","Test works");
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+
 
 }
